512145d28d3584b437a790f5ad18b65ad2e00793
Adolfo <adolfo@rewrite.com.br>
2024-01-02 20:45:58 -0400

    feat: model and mock

---
+++ src/app/Currency.ts
@@ -0,0 +1,6 @@
+export interface Currency {
+  rate: number;
+  fullName: string;
+  name: string;
+  symbol: string;
+}
---
+++ src/app/mock-currency.ts
@@ -0,0 +1,9 @@
+import { Currency } from "./Currency";
+
+export const CURRENCIES: Currency[] = [
+  { rate: 1, fullName: "US Dollar", name: "USD", symbol: "$" },
+  { rate: 1.001, fullName: "Euro", name: "EUR", symbol: "€" },
+  { rate: 4.725, fullName: "Polish Zloty", name: "PLN", symbol: "zł" },
+  { rate: 7.524, fullName: "Croatian Kuna", name: "HRK", symbol: "kn" },
+  { rate: 0.869, fullName: "British Pound", name: "GBP", symbol: "£" },
+];
