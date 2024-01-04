79762087ff2923d28bd24ac51dd66cdd9dd5eb04
Adolfo <adolfo@rewrite.com.br>
2024-01-03 20:53:32 -0400

    feat: ReactiveFormsModule added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
 })
 export class AppModule {}
