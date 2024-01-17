e6a29cc4d53c9b1b3526ff1346ac46d5568a098b
Adolfo <adolfo@rewrite.com.br>
2024-01-17 19:49:31 -0400

    feat: HttpClient imported and main.ts changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterOutlet } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
 import { FormControl, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
@@ -14,4 +15,18 @@ export class AppComponent {
   title = "Currency Converter";
 
   amount = new FormControl("1.00");
+
+  constructor(private http: HttpClient) {}
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    this.http
+      .post(
+        "https://angular-making-http-request-default-rtdb.firebaseio.com/posts.json",
+        postData
+      )
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 /// <reference types="@angular/localize" />
 
 import { bootstrapApplication } from "@angular/platform-browser";
-import { appConfig } from "./app/app.config";
+import { provideHttpClient } from "@angular/common/http";
+import { provideRouter } from "@angular/router";
+import { routes } from "../src/app/app.routes";
 import { AppComponent } from "./app/app.component";
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
-);
+bootstrapApplication(AppComponent, {
+  providers: [provideHttpClient(), provideRouter(routes)],
+});
