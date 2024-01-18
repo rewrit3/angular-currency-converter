f258386cf6786853af052733d28f5eda51b240df
Adolfo <adolfo@rewrite.com.br>
2024-01-17 20:47:08 -0400

    feat: http.get on currencies api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterOutlet } from "@angular/router";
 import { HttpClient } from "@angular/common/http";
 import { FormControl, ReactiveFormsModule } from "@angular/forms";
+// import { map } from "rxjs/operators";
 
 @Component({
   selector: "app-root",
@@ -11,22 +12,22 @@ import { FormControl, ReactiveFormsModule } from "@angular/forms";
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "Currency Converter";
 
   amount = new FormControl("1.00");
 
   constructor(private http: HttpClient) {}
 
-  onCreatePost(postData: { title: string; content: string }) {
-    // Send Http request
+  ngOnInit() {
+    this.fetchCurrenciesAndCountries();
+  }
+
+  private fetchCurrenciesAndCountries() {
     this.http
-      .post(
-        "https://angular-making-http-request-default-rtdb.firebaseio.com/posts.json",
-        postData
-      )
-      .subscribe((responseData) => {
-        console.log(responseData);
+      .get("https://restcountries.com/v3.1/all?fields=currencies")
+      .subscribe((currencies) => {
+        console.log(currencies);
       });
   }
 }
